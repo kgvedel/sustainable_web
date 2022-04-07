3db251b133a089e6aaf184c16d951043bb72a761
KatrineVedel <katr499k@stud.kea.dk>
2022-04-07 12:44:49 +0200

    Done

---
+++ script.js
@@ -0,0 +1,8 @@
+//1 grib submit
+//2 preventDefault
+//løse url fra bruger
+//fetch fra lokal fil
+
+const submit_btn = document.querySelector(".calc_btn");
+
+fetch("kea.json").then(res=>res.json()).then(console.log);
