41aa95ce05a0eafdd0d7194946317777b09ae101
mathilde <math68n8@stud.kea.dk>
2022-04-09 17:05:52 +0200

    visedata igang

--- script.js
+++ script.js
@@ -7,17 +7,76 @@
 
 //vi mangler: Clone, åbne nyt site ved calculate og forms
 const form = document.querySelector("form.calculator");
+const allData = [];
+const dataCarbon = {
+    energy: "",
+    co2: "",
+    green: true
+   
+};
 
-form.addEventListener("submit", calculate);
+function start(){
+    form.addEventListener("submit", calculate);
+}
+
+
+
+const user_url = form.elements.url.value;
 
 function calculate(event) {
-    const user_url = form.elements.url.value;
-    
+   
+  /*   
     const pageInsightApiKey = "AIzaSyB5TMLidzXZG4KFFbQjWVmGv1bfUYPrDGg";
-  let pageSpeed =  fetch( `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${user_url}&key=${pageInsightApiKey}`).then(res => res.json()).then(console.log);
+  let pageSpeed =  fetch( `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${user_url}&key=${pageInsightApiKey}`).then(res => res.json()).then(console.log); */
     
-  let carbon =  fetch(`https://kea-alt-del.dk/websitecarbon/site/?url=${user_url}`).then(res => res.json()).then(console.log);
-   event.preventDefault();
+  event.preventDefault();
+  loadJsonCarbon();
+}
+
+function loadJsonCarbon(){
+    fetch(`kea.json`).then(res => res.json()).then(jsonData =>{prepareObjects(jsonData)});
+ 
+   /*  fetch(`https://kea-alt-del.dk/websitecarbon/site/?url=${user_url}`).then(res => res.json()).then(console.log);
+ */
+}
+
+
+function prepareObjects(jsonObject){
+   
+        const carbon = Object.create(dataCarbon);
+
+carbon.energy = jsonObject.statistics.energy;
+carbon.co2 = jsonObject.statistics.co2;
+carbon.green = jsonObject.green;
+
+
+        console.log(carbon);
+
+allData.push(carbon);
+console.log(allData);
+}
+
+
+start();
+
+function displayList(){
+//clearing the list
+   document.querySelector("#list tbody").innerHTML= "";
+   allData.forEach(displayData);
+
+}
+
+function displayData(carbon){
+    const clone = document.querySelector("template #carbon_template").content.cloneNode(true);
+
+    clone.querySelector("[data-field=energy]").textContent = carbon.energy;
+    clone.querySelector("[data-field=co2]").textContent = carbon.co2;
+    clone.querySelector("[data-field=green]").textContent = carbon.green;
+
+    console.log
+
+    document.querySelector("#list tbody").appendChild(clone);
+
 }
 
 /* function showDetails(){
