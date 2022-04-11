7fc783b2fd29345482745945a5ed8c8c10ef546a
KatrineVedel <katr499k@stud.kea.dk>
2022-04-11 17:50:38 +0200

    hej hej

--- script.js
+++ script.js
@@ -9,16 +9,11 @@
 const form = document.querySelector("#calculator");
 
 //import{loadJsonCarbon.from}
-
-
-
  form.addEventListener("submit", calculate);
 
 // const user_url = form.elements.url.value;
 
 function calculate(event) {
-
-
     /*   
       const pageInsightApiKey = "AIzaSyB5TMLidzXZG4KFFbQjWVmGv1bfUYPrDGg";
     let pageSpeed =  fetch( `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${user_url}&key=${pageInsightApiKey}`).then(res => res.json()).then(console.log); */
@@ -28,6 +23,23 @@ function calculate(event) {
     location.href = "resolve.html";
 
     
+}
+
+
+const APIKEY = "6254481067937c128d7c96cc";
+const endpoint = "https://sustainable-485c.restdb.io/rest/company ";
+
+function post(payload) {
+  fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "x-apikey": APIKEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .then((data) => init());
 }
 
 
